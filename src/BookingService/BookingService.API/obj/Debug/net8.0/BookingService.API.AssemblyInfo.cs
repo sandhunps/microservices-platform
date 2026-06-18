7d5d43c1a00ec3b17cae1efd6cde34b75dc84fff
Sandhun <Sandhun47@gamil.com>
2026-06-19 00:07:05 +0530

    feat: add Dockerfiles and docker-compose for all 3 services

--- src/BookingService/BookingService.API/obj/Debug/net8.0/BookingService.API.AssemblyInfo.cs
+++ src/BookingService/BookingService.API/obj/Debug/net8.0/BookingService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3553adffa8a8080442fa1f62f4684629c82e972b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
